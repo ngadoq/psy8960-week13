7b3140bab6804293d39609e03581d2fe5451dd99
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-04-29 19:19:12 -0500

    final commit

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -23,4 +23,29 @@ dbGetQuery(conn, "SELECT COUNT(employee_id) AS n_managers
                   FROM datascience_8960_table;")
 
 # Display the total number of unique managers (unique by id number)
+dbGetQuery(conn, "SELECT COUNT(DISTINCT employee_id) AS n_managers_unique
+                  FROM datascience_8960_table;")
+
+# Display a summary of the number of managers split by location, but only include those who were not originally hired as managers
+dbGetQuery(conn, "SELECT city, COUNT(employee_id) AS n_managers_city
+                  FROM datascience_8960_table
+                  WHERE manager_hire = 'N'
+                  GROUP BY city;")
+
+# Display the average and standard deviation of number of years of employment split by performance level (bottom, middle, and top)
+dbGetQuery(conn, "SELECT performance_group, AVG(yrs_employed) AS mean_yrs_employed, STDDEV(yrs_employed) AS sd_yrs_employed
+                  FROM datascience_8960_table
+                  GROUP BY performance_group;")
+
+# Display the location and ID numbers of the top 3 managers from each location, in alphabetical order by location and then descending order of test score. If there are ties, include everyone reaching rank 
+
+dbGetQuery(conn, "WITH rank_manager AS (
+  SELECT *, 
+  RANK() OVER(PARTITION BY city ORDER BY test_score DESC) AS rank_number
+  FROM datascience_8960_table)
+  SELECT city, employee_id
+  FROM rank_manager
+  WHERE rank_number IN (1,2,3);")
+
+
 
